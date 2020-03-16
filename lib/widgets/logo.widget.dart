9e7c0239c95722123ae33d7338c525ec76074651
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-03-16 05:02:32 -0300

    add logo.widget

--- lib/widgets/logo.widget.dart
+++ lib/widgets/logo.widget.dart
@@ -1,10 +1,16 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class logo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
+// class logo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Theme.of(context).primaryColor,
+//       body: ListView(
+//         children: <Widget>[
+//           SizedBox (height: 60)
+
+//         ],
+//       )
+//     );
+//   }
+// }
