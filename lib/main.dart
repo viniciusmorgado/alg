8d494dba939858d34da1491fd38c60a11bd03787
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-03-15 21:26:47 -0300

    add new widgets directory

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,61 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Esse widget é a raiz da sua aplicação e é herança da superclasse Statelesswidget
   @override
+  // Aqui o parametro "widget" define que estavamos trabalhando dentro de um widget, e o método build() é o responsável por construir na tela nosso layout.
   Widget build(BuildContext context) {
+    // Aqui nós retornamos o método MaterialApp que será responsável por definir caracteristicas básicas da nossa aplicação como titulo (não visivel ao usuário) e paleta de cores.
     return MaterialApp(
+      // Remove a barra de debug da aplicação.
+      debugShowCheckedModeBanner: false,
+      // Title é o parametro que nosso método MaterialApp usa para definir o titulo da aplicação em seu core, o mesmo não é visivel para o usuário mais é definido por um questão semântica.
       title: 'Álcool ou Gasolina',
+      // theme é o parametro dentro de nosso método MaterialApp que é capaz de receber métodos para personalização geral da nossa aplicação, como por exemplo alteração  a paleta de cores, no exemplo abaixo ele recebe o método ThemeData() que possui parametros como o primarySwtch: que nos permite alterar a paleta de cores geral que será usada por toda a aplicação.
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.purple,
+        // Aletra a paleta de cores de acordo com a definida.
+        primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Álcool ou Gasolina'),
+      // Invoca a função principal HomePage que será construida logo abaixo com a nossa tela principal.
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// Classe principal que constroe o corpo da nossa aplicação sobre um widget Stateless.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Scaffold é à função responsável por implementar a estrutura base do layout Material Design, diferente do container que é responsável por ser uma forma conveniênte de alinhar tamanho e posicionamento de diferentes widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        // Utilizando o parametro backgroundcColor nós definimos uma cor de fundo para nossa aplicação e com o método Theme.of(context) nós requisitamos a utilização da paleta de cores primária definida no MyApp.
+        backgroundColor: Theme.of(context).primaryColor,
+
+        // body é o marametro do Scaffold que nos permite definir as carasteristicas do corpo do nosso widget, similar ao <body> do HTML porém menos abrangênte já que o parametro se aplica apenas o widget da classe HomePage.
+        // O método ListView é utilizado para organizar items de uma lista de forma prática, em nosso caso ele será muito útil já que nossos widgets principais ficaram dispotos um embaixo do outro.
+        body: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            // Método SizedBoz define um tamanho para o widget Listview.
+            SizedBox(height: 60),
+            // Método Image está utilizando a função asset para acessar uma imagem especifica dentro do diretório do projeto e imprimi-lo na tela, nessa caso a imagem da nossa logo.
+            Image.asset(
+              "assets/images/aog-white.png",
+              // parametro height define o tamanho da imagem.
+              height: 80,
             ),
+            SizedBox(height: 10),
+            // Método Text está criando o texto "Àlcool e Gasoline" e utilizando parametros já vistos anteriormente definindo as propriedades desse widget de texto como fonte, cor etc.
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              "Álcool ou Gasolina",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                //fontFamily: "BigShouldersDisplay"
+              ),
+              // textAlign o parametro está alinhando o widget de texto que foi definido anteriormente incluindo todas suas propriedades.
+              textAlign: TextAlign.center,
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
 }
