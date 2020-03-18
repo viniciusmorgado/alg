95beac3f67c49db44ca3eda412136877038fae1a
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-03-18 17:39:58 -0300

    Add calc button to main page

---
+++ lib/widgets/button.widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text("Calcular"),
+      onPressed: () {},
+    );
+  }
+}
